df30c2de90f871306be573cce40e1c66f88a0b5c
juancarlosasensio <jasensio@ufm.edu>
2020-03-20 10:44:55 +0000

    Generate Todo model and associate with User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
+  has_many :todos
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
