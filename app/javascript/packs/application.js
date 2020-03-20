428d5c3876e3ceeb598a3b54dd43d535252f58ad
juancarlosasensio <jasensio@ufm.edu>
2020-03-20 12:07:48 +0000

    Install Stimulus.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,11 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
+
+import "controllers"
+
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+const application = Application.start()
+const context = require.context("controllers", true, /.js$/)
+application.load(definitionsFromContext(context))
