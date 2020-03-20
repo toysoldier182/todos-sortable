485c94db24b8610c142e1fab451cd142d8785256
juancarlosasensio <jasensio@ufm.edu>
2020-03-20 10:52:06 +0000

    Add basic CRUD actions

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,39 @@
+class TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :destroy]
+
+  def index
+    @todos = Todo.where(user_id == current_user.id)
+  end
+
+  def show; end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    @todo.user_id = current_user.id
+    if @todo.save
+      redirect_to todos_path
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    @todo.destroy
+    redirect_to
+  end
+
+  private
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+
+  def todo_params
+    params.require(:todo).permit(:name, :due_date, :completed)
+  end
+
+end
